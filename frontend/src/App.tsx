3422c71ba469512e65fafbe1c2091e7fabec3fd3
[Aaditya1273] <aadityaiitian1515@gmail.com>
2026-01-23 17:46:29 +0530

    newly

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <>
+            <div className="card">
+                <h1>AURA</h1>
+                <p>Agentic Universal Routing & Authorization</p>
+            </div>
+        </>
+    )
+}
+
+export default App
